b9882e8fc79c87e3551ecd66b7a57ae9d9c8078c
Tiger <atlantisspeed@gmail.com>
2019-04-13 21:31:06 +0800

    更换为RecyclerView

--- app/build/generated/source/buildConfig/debug/com/example/demo/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/demo/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.demo;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.demo";
+  public static final String APPLICATION_ID = "com.volvo";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
